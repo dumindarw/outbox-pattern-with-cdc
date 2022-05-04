ab50f8feaaf17cb90a660c3febb799e51d0a4323
Rajitha Wanninayake <rajitha.wanninayake@mcmedisoft.com>
2022-05-04 10:29:25 +0530

    minor css change

--- location-client/src/index.js
+++ location-client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import 'leaflet/dist/leaflet.css';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
